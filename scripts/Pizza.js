e03f494a05fac8fe32210c5e909c9ad6db04323a
Vera <veraglad85@gmail.com>
2025-04-28 15:41:59 +0300

    Pizza

--- scripts/Pizza.js
+++ scripts/Pizza.js
@@ -1,15 +1,24 @@
-console.log(10<20);
+
+function iInfo() {
+    let i = document.getElementById('i-text');
+    i.style.display = (i.style.display === 'none') ? 'block' : 'none';
+}
+
 
 function plus() {
     let täyte1, täyte2, täyte3, täyte4, täyte5, result;
-    täyte1=document.getElementById("A").value 2;
-    täyte2=document.getElementById("K").value 2;
+    täyte1=document.getElementById("A").value;
+    täyte2=document.getElementById("K").value;
     täyte3=document.getElementById("P").value;
     täyte4=document.getElementById("Au").value;
     täyte5=document.getElementById("T").values;
     result=täyte1+täyte2+täyte3+täyte4+täyte5;
+    täyte1=2;
     document.getElementById ("out")
     .innerHTML=result;
 }
+
+
+
 
 
